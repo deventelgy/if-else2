30b9f395f94b8ca8e0e675138c0c8bbd7d5478cc
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-23 14:39:55 -0500

    condicional

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int val1=7;
+        int val2=5;
+        int maxValue=val1>val2? val1: val2;
+        System.out.println(maxValue);
+    }
+}
